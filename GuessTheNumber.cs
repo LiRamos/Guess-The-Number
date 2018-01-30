da5f561ba2a8e70ad911f590a953976edb1ad32f
LissetteMarie22 <ramoslissette22@yahoo.com>
2018-01-30 12:38:40 -0500

    A game of guess the number where the number is randomly generated.

---
+++ GuessTheNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number = new Random();
+            int mystery = number.Next(1,999);
+            Console.WriteLine("Hello , Let's play a game of Guess The Number! I have a number between 1 and 999, you have 4 tries to guess it.");
+            for (int i = 4; i > 0; i--) {
+                int guess = int.Parse(Console.ReadLine());
+                if (guess == mystery) {
+                    Console.WriteLine("You guessed it, you win! The number was {0}" , mystery);
+                    break;
+                    }
+                else
+                {
+                Console.WriteLine("Nope, that was not the number, try again!"); 
+                }
+
+            }
+            Console.WriteLine("Looks like you did not guess the right number, it was actually {0}" , mystery);
+        }
+    }
+}
